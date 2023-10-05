839c255decb561f245efe6f7be7d2f3e08bc6fdc
suranjan.maity <suranjanmaity4@gmail.com>
2023-10-05 12:28:36 +0530

    Select operator - Querry Syntax

--- LINQExample_1/Program.cs
+++ LINQExample_1/Program.cs
@@ -9,16 +9,29 @@ namespace LINQExample_1
         {
             List<Employee> employeeList = Data.GetEmployees();
             List<Department> departmentList = Data.GetDepartments();
+            //Select and Where operators - Method Syntax
             // using Select Linq operator
-            var result = employeeList.Select(e => new
-            {
-                FullName = e.FirstName + " " + e.LastName,
-                AnnualSalary = e.AnualSalary
-            }).Where(e=>e.AnnualSalary >= 50000) ;
+            //var result = employeeList.Select(e => new
+            //{
+            //    FullName = e.FirstName + " " + e.LastName,
+            //    AnnualSalary = e.AnualSalary
+            //}).Where(e=>e.AnnualSalary >= 50000) ;
+            //foreach (var res in result)
+            //{
+            //    Console.WriteLine($"Full Name : {res.FullName}");
+            //    Console.WriteLine($"Annual Salary : {res.AnnualSalary}");
+            //}
+
+            //Select and Where operators - Querry Syntax
+            var result = from emp in employeeList
+                         select new
+                         {
+                             FullName = emp.FirstName + " " + emp.LastName,
+                             AnnualSalary = emp.AnualSalary
+                         };
             foreach (var res in result)
             {
-                Console.WriteLine($"Full Name : {res.FullName}");
-                Console.WriteLine($"Annual Salary : {res.AnnualSalary}");
+                Console.WriteLine($"{res.FullName,-20} {res.AnnualSalary,10}");
             }
         }
     }
