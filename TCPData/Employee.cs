6e6ff96ef61820dc778cd10470bc211ea048f91d
suranjan.maity <suranjan.maity@NOI-PF3THR26>
2023-10-05 11:41:47 +0530

    Add project files.

---
+++ TCPData/Employee.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TCPData
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public decimal AnualSalary { get; set; }
+        public bool IsManager { get; set; }
+        public int DepartmentId { get; set; }
+    }
+}
---
+++ ThePretendCompanyApplication/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using TCPData;
+using TCPExtensions;
+using System.Linq;
+
+namespace ThePretendCompanyApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Employees who are manager...");
+            List<Employee> employeeList = Data.GetEmployees();
+            var filteredEmployees = employeeList.Filter(emp => emp.IsManager == true);
+            foreach (var employee in filteredEmployees)
+            {
+                Console.WriteLine($"First Name : {employee.FirstName}");
+                Console.WriteLine($"Last Name : {employee.LastName}");
+                Console.WriteLine($"Annual Salary : {employee.AnualSalary}");
+                Console.WriteLine($"Manager : {employee.IsManager}");
+                Console.WriteLine();
+            }
+            
+            Console.WriteLine("Employees who are earning more than 50000...");
+            filteredEmployees = employeeList.Filter(emp => emp.AnualSalary > 50000);
+            foreach (var employee in filteredEmployees)
+            {
+                Console.WriteLine($"First Name : {employee.FirstName}");
+                Console.WriteLine($"Last Name : {employee.LastName}");
+                Console.WriteLine($"Annual Salary : {employee.AnualSalary}");
+                Console.WriteLine($"Manager : {employee.IsManager}");
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("Finance and Human Resource Department...");
+            List<Department> departmentList = Data.GetDepartments();
+            var filteredDepartment = departmentList.Filter(dept => dept.ShortName=="HR" || dept.ShortName == "FN");
+            foreach (var department in filteredDepartment)
+            {
+                Console.WriteLine($"Id : {department.Id}");
+                Console.WriteLine($"Short Name : {department.ShortName}");
+                Console.WriteLine($"Long Name : {department.LongName}");
+                Console.WriteLine();
+            }
+            // filtering the data using where method of System.Linq namespace
+            Console.WriteLine("Finance and Human Resource Department...");
+            var whereDepartment = departmentList.Where(dept=> dept.ShortName=="HR" || dept.ShortName == "FN" );
+            foreach (var department in filteredDepartment)
+            {
+                Console.WriteLine($"Id : {department.Id}");
+                Console.WriteLine($"Short Name : {department.ShortName}");
+                Console.WriteLine($"Long Name : {department.LongName}");
+                Console.WriteLine();
+            }
+        }
+    }
+}
